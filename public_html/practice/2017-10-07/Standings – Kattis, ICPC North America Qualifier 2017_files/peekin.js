568631f64e2169f5a334e7539bfaf1acbf0f8930
zhengdw <7789863+Zhengdw@users.noreply.github.com>
2023-08-02 17:43:05 -0500

    scraped html of old website

---
+++ public_html/practice/2017-10-07/Standings – Kattis, ICPC North America Qualifier 2017_files/peekin.js
@@ -0,0 +1,5 @@
+;(function() {
+    console.log("%c❗️ Peekin is now Upscope.io,", 'color: #49AAE2; font-weight: 400; font-size: 18px; font-family: "Avenir"');
+    console.log("%c   ...and the old Peekin code is no longer working.", 'font-size: 14px;');
+    console.log("%c   Create your https://upscope.io/?ref=cons account today to start your 14 days free trial.", 'color: #49AAE2;font-size: 14px;');
+})();
